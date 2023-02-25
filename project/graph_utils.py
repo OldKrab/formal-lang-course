ddf2dd304eddf581e5abb4a852cc9e3968178dc8
OldKrab <4shushakov@gmail.com>
2023-02-25 22:42:31 +0300

    done

---
+++ project/graph_utils.py
@@ -0,0 +1,32 @@
+import cfpq_data
+import networkx.classes.multidigraph as nx_mdg
+import networkx.drawing.nx_pydot as nx_pydot
+from typing import Set, Tuple
+
+def _load_graph(name: str) -> nx_mdg.MultiDiGraph:
+    path = cfpq_data.download(name)
+    return cfpq_data.graph_from_csv(path)
+
+
+def _get_graph_vertexes_number(gr: nx_mdg.MultiDiGraph) -> int:
+    return gr.number_of_nodes()
+
+
+def _get_graph_edges_number(gr: nx_mdg.MultiDiGraph) -> int:
+    return gr.number_of_edges()
+
+
+def _get_graph_unique_labels(gr: nx_mdg.MultiDiGraph) -> Set[str]:
+    return {data["label"] for (_, _, data) in gr.edges(data=True)}
+
+
+def get_graph_info(name: str) -> Tuple[int, int, Set[str]]:
+    graph = _load_graph(name)
+    return (_get_graph_vertexes_number(graph),
+            _get_graph_edges_number(graph),
+            _get_graph_unique_labels(graph))
+
+
+def save_labeled_two_cycles_graph(n: int, m: int, labels: Tuple[str, str], path: str) -> None:
+    gr = cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
+    nx_pydot.write_dot(gr, path)
---
+++ tests/test_get_graph_info.py
@@ -0,0 +1,22 @@
+import pytest
+import project.graph_utils as graph_utils  # on import will print something from __init__ file
+
+
+def setup_module(module):
+    pass
+
+
+def teardown_module(module):
+    pass
+
+
+def test_ls():
+    expected = (1687, 1453, {'d', 'a'})
+    actual = graph_utils.get_graph_info("ls")
+    assert expected == actual
+
+def test_gzip():
+    expected = (2687, 2293, {'a', 'd'})
+    actual = graph_utils.get_graph_info("gzip")
+    assert expected == actual
+
