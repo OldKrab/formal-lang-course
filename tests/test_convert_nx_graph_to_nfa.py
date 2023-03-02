59517568ee5c8bc2c1852d9a54e2b9bead0dfbd0
OldKrab <4shushakov@gmail.com>
2023-03-02 22:52:21 +0300

    simplify tests

--- tests/test_convert_nx_graph_to_nfa.py
+++ tests/test_convert_nx_graph_to_nfa.py
@@ -15,14 +15,14 @@ def test_simple_graph_all_start_final():
         [(0, 1, {"label": "a"}), (1, 2, {"label": "b"}), (2, 3, {"label": "c"})]
     )
     nfa = convert_nx_graph_to_nfa(gr)
-    assert nfa.accepts(["a"])
-    assert nfa.accepts(["b"])
-    assert nfa.accepts(["c"])
-    assert nfa.accepts(["a", "b", "c"])
-    assert nfa.accepts(["b", "c"])
-    assert not nfa.accepts(["d"])
-    assert not nfa.accepts(["a", "b", "d"])
-    assert not nfa.accepts(["a", "b", "c", "d"])
+    assert nfa.accepts("a")
+    assert nfa.accepts("b")
+    assert nfa.accepts("c")
+    assert nfa.accepts("abc")
+    assert nfa.accepts("bc")
+    assert not nfa.accepts("d")
+    assert not nfa.accepts("abd")
+    assert not nfa.accepts("abcd")
 
 
 def test_cycle_graph_all_start_final():
@@ -30,23 +30,23 @@ def test_cycle_graph_all_start_final():
         [(0, 1, {"label": "a"}), (1, 2, {"label": "b"}), (2, 0, {"label": "c"})]
     )
     nfa = convert_nx_graph_to_nfa(gr)
-    assert nfa.accepts(["a"])
-    assert nfa.accepts(["b"])
-    assert nfa.accepts(["c"])
-    assert nfa.accepts(["a", "b", "c"])
-    assert nfa.accepts(["b", "c", "a"])
-    assert nfa.accepts(["a", "b", "c", "a", "b", "c", "a"])
-    assert not nfa.accepts(["a", "b", "c", "a", "b", "c", "c"])
+    assert nfa.accepts("a")
+    assert nfa.accepts("b")
+    assert nfa.accepts("c")
+    assert nfa.accepts("abc")
+    assert nfa.accepts("bca")
+    assert nfa.accepts("abcabca")
+    assert not nfa.accepts("abcabcc")
 
 
 def test_graph_with_loop_edge_all_start_final():
     gr = nx.MultiDiGraph([(0, 1, {"label": "a"}), (1, 1, {"label": "b"})])
     nfa = convert_nx_graph_to_nfa(gr)
-    assert nfa.accepts(["a"])
-    assert nfa.accepts(["b"])
-    assert nfa.accepts(["b", "b"])
-    assert nfa.accepts(["a", "b", "b", "b"])
-    assert not nfa.accepts(["a", "b", "b", "a"])
+    assert nfa.accepts("a")
+    assert nfa.accepts("b")
+    assert nfa.accepts("bb")
+    assert nfa.accepts("abbb")
+    assert not nfa.accepts("abba")
 
 
 def test_graph_with_set_start_states():
@@ -59,12 +59,12 @@ def test_graph_with_set_start_states():
         ]
     )
     nfa = convert_nx_graph_to_nfa(gr, [0, 3])
-    assert nfa.accepts(["a"])
-    assert nfa.accepts(["d"])
-    assert nfa.accepts(["d", "d"])
-    assert nfa.accepts(["a", "b", "c", "d", "d"])
-    assert not nfa.accepts(["b"])
-    assert not nfa.accepts(["b", "c"])
+    assert nfa.accepts("a")
+    assert nfa.accepts("d")
+    assert nfa.accepts("dd")
+    assert nfa.accepts("abcdd")
+    assert not nfa.accepts("b")
+    assert not nfa.accepts("bc")
 
 
 def test_graph_with_set_final_states():
@@ -77,14 +77,14 @@ def test_graph_with_set_final_states():
         ]
     )
     nfa = convert_nx_graph_to_nfa(gr, None, [1, 3])
-    assert nfa.accepts(["a"])
-    assert nfa.accepts(["a", "b", "c"])
-    assert nfa.accepts(["d"])
-    assert nfa.accepts(["d", "d"])
-    assert nfa.accepts(["a", "b", "c", "d", "d"])
-    assert nfa.accepts(["b", "c", "d"])
-    assert not nfa.accepts(["a", "b"])
-    assert not nfa.accepts(["b"])
+    assert nfa.accepts("a")
+    assert nfa.accepts("abc")
+    assert nfa.accepts("d")
+    assert nfa.accepts("dd")
+    assert nfa.accepts("abcdd")
+    assert nfa.accepts("bcd")
+    assert not nfa.accepts("ab")
+    assert not nfa.accepts("b")
 
 
 def test_graph_with_set_start_and_final_states():
@@ -97,11 +97,11 @@ def test_graph_with_set_start_and_final_states():
         ]
     )
     nfa = convert_nx_graph_to_nfa(gr, [0], [2])
-    assert nfa.accepts(["a", "b"])
-    assert not nfa.accepts(["d"])
-    assert not nfa.accepts(["d", "d"])
-    assert not nfa.accepts(["a", "b", "c", "d", "d"])
-    assert not nfa.accepts(["b", "c", "d"])
-    assert not nfa.accepts(["a"])
-    assert not nfa.accepts(["b"])
-    assert not nfa.accepts(["a", "b", "c"])
+    assert nfa.accepts("ab")
+    assert not nfa.accepts("d")
+    assert not nfa.accepts("dd")
+    assert not nfa.accepts("abcdd")
+    assert not nfa.accepts("bcd")
+    assert not nfa.accepts("a")
+    assert not nfa.accepts("b")
+    assert not nfa.accepts("abc")
